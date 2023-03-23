f709e82639e02e153937b6d2009d061f8c91e7f3
Ihor Poliakov <piv2003@gmail.com>
2023-03-23 22:15:52 +0200

    working with component Profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,2 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import user from 'data/user.json';
+import { Profile } from 'components/Profile/Profile';
