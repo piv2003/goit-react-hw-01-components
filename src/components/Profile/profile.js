c9e89a78d345b254d60bd9441d9961928f8521a8
Ihor Poliakov <piv2003@gmail.com>
2023-06-24 23:35:33 +0200

    fix errors

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -53,7 +53,5 @@ Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  // followers: PropTypes.number.isRequired,
-  // views: PropTypes.number.isRequired,
-  // likes: PropTypes.number.isRequired,
+  stats: PropTypes.object.isRequired,
 };
