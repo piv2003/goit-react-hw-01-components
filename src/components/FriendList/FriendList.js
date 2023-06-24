9b549a9b327ac19a80a9dbaa762d245d906de6cc
Ihor Poliakov <piv2003@gmail.com>
2023-06-24 10:25:27 +0200

    Update FriendList.js

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -14,7 +14,6 @@ export const FriendList = ({ friends }) => {
             avatar={avatar}
             name={name}
             isOnline={isOnline}
-            id={id}
           />
         )
       )}
